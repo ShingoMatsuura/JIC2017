e3031ddff35a251be4b74956960a10ea714722ac
ShingoMatsuura <s.matsuura@nakashima.co.jp>
2017-09-27 23:02:22 +0900

    save to usb memory

--- companion/start_capture.sh
+++ companion/start_capture.sh
@@ -6,18 +6,16 @@ set -x
 QUALITY=100
 
 # create datetime directory for images
-DATETIME_DIR=$(date "+%Y%m%d_%H-%M-%S")
-mkdir /mnt/img/${DATETIME_DIR}
+DATETIME_DIR=$(date +"%Y%m%d_%H-%M-%S")
+mkdir /media/usb0/${DATETIME_DIR}
 
-FILE_NAME=$(date "%Y%m%d%H%M%S%2N")
 #ln -s latest_img.jpg fake_chameleon.pgm
 
 ## TODO: check param, ISO:400
-raspistill -w 1280 -h 960 -n -r -ISO 400 -q $QUALITY -v -dt -t 3600000 -tl 0 -o /mnt/img/${DATETIME_DIR}/${FILE_NAME}Z.jpg
+raspistill -n -r -ISO 400 -q $QUALITY -v -dt -t 3600000 -tl 0 -o /media/usb0/${DATETIME_DIR}/%dZ.jpg
 
 #change to /mnt
-cd /mnt
+#cd /media/usb0
 
 #start mavproxy
-mavproxy.py --master localhost:14655 --aircraft MyCopter
-
+#mavproxy.py --master localhost:14655 --aircraft MyCopter
