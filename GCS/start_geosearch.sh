eb2cc3c6039ad77d29b47dd60b3f8fb15ff1f5d3
Randy Mackay <rmackay9@yahoo.com>
2017-09-29 14:20:24 +0900

    GCS: start-geosearch starts analysis immediately

--- GCS/start_geosearch.sh
+++ GCS/start_geosearch.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-geosearch.py --sensorwidth 3.674 --lens 40 --minscore 1000 --roll-stabilised --time-offset 1 --mavlog $1/mav.tlog $1
+geosearch.py --sensorwidth 3.674 --lens 40 --minscore 1000 --roll-stabilised --time-offset 1 --start --mavlog $1/mav.tlog $1
