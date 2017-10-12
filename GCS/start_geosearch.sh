e0ce7fca53d53cf73089858d192411cec68fed1f
Shingo Matsuura <s.matsuura@nakashima.co.jp>
2017-10-13 07:17:00 +0900

    change minscore to 100 and add vehicle-type option

--- GCS/start_geosearch.sh
+++ GCS/start_geosearch.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-geosearch.py --sensorwidth 3.674 --lens 40 --minscore 1000 --roll-stabilised --time-offset 1 --start --mavlog $1/mav.tlog $1
+geosearch.py --sensorwidth 3.674 --lens 40 --minscore 100 --roll-stabilised --time-offset 1 --start --vehicle-type Copter --mavlog $1/mav.tlog $1 
