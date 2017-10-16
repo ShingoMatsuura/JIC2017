3033ecb8b7a8f3a6190526e84459c8dac973044d
Randy Mackay <rmackay9@yahoo.com>
2017-10-16 16:13:26 +0900

    companion: autostart_rpi_capture starts bayer_to_jpg

--- companion/start_rpi_capture/autostart_rpi_capture.sh
+++ companion/start_rpi_capture/autostart_rpi_capture.sh
@@ -5,7 +5,17 @@ set -e
 set -x
 
 pushd ~apsync/start_rpi_capture
+
+# directory for images
+SOURCE_DIR=/var/images
+DATETIME_DIR=$(date +"%Y%m%d_%H-%M-%S")
+DEST_DIR=/media/usb0/${DATETIME_DIR}
+
+# start rpi capture
 screen -L -d -m -S rpi_capture -s /bin/bash ./start_rpi_capture.sh >start_rpi_capture.log 2>&1
+
+# start bayer to jpg conversion
+screen -L -d -m -S bayer_to_jpg -s /bin/bash ./bayer_to_jpg.sh ${SOURCE_DIR} ${DEST_DIR} >bayer_to_jpg.log 2>&1
 
 exit 0
 
